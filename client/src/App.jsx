5cef990714727ed6b546ea49d535a8d4eb4fdf48
Eric Afes <eriicafes@gmail.com>
2023-03-11 23:16:14 +0100

    Init client application

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Olist Ecommerce Dataset</h1>
+    </div>
+  )
+}
+
+export default App
